15fa5689bec4d1af6eb139014446b823f08eab90
user <morevstas02@mail.ru>
2025-05-21 16:38:31 +0300

    Один ко многим с юзером и постами, добавление постов и юзеров

--- Models/models.py
+++ Models/models.py
@@ -5,14 +5,22 @@ from sqlalchemy.testing.schema import mapped_column
 from DataBase.DataBase import Base
 
 
+
+
+
+
+
+
+
+
 class Person(Base):
     __tablename__ = "people"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     age: Mapped[int] = mapped_column(Integer,nullable=True)
     email: Mapped[str] = mapped_column(String(50),nullable = True)
-    peoples: Mapped['Post'] = relationship(back_populates='person')
-    profile: Mapped['Profile'] = relationship('Profile',back_populates='person', uselist=False)
+    # связб с постом
+    post = relationship('Post',back_populates='person_post')
 
     def __str__(self):
         return f'{self.__class__.__name__}(id = {self.id} name = {self.name},age = {self.age}, email = {self.email})'
@@ -26,10 +34,10 @@ class Post(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(Text)
+    # cсылка на его id
     person_id: Mapped[int] = mapped_column(ForeignKey('people.id'))
-
-    person : Mapped['Person'] = relationship(back_populates='peoples')
-
+    # обратная ссылка один ко многим с человеком
+    person_post = relationship('Person',back_populates='post')
 
 
 class Profile(Base):
@@ -38,8 +46,8 @@ class Profile(Base):
     first_name: Mapped[str] = mapped_column(String(40))
     last_name: Mapped[str] = mapped_column(String(40))
     bio: Mapped[str]  = mapped_column(String(100))
-    person_id :Mapped[int] = mapped_column(ForeignKey('people.id'))
-    person : Mapped['Person'] = relationship(back_populates='profile')
+
+
 
     def __str__(self):
         return (f'{self.__class__.__name__}(id = {self.id} first_name = {self.first_name},last_name = {self.last_name},'
--- crud/crud.py
+++ crud/crud.py
@@ -1,38 +1,53 @@
 import asyncio
+from asyncio import run
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from DataBase.DataBase import async_session_maker, engine
-from Models.models import Person,Profile
-# вывод профиля + юзера
-async def selest(session: AsyncSession):
-    result = await session.execute(select(Person).options(selectinload(Person.profile)))
-    return result.scalars().all()
+from Models.models import Person, Profile, Post
 
 
+def connection(method):
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                # Явно не открываем транзакции, так как они уже есть в контексте
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()  # Откатываем сессию при ошибке
+                raise e  # Поднимаем исключение дальше
+            finally:
+                await engine.dispose()  # Закрываем сессию
 
-# cессия
-async def main():
-    async with async_session_maker() as session:
-        try:
-          result = await selest(session=session)
-          for i in result:
-              print(i)
-              if i.profile:
-                  print(f'Profile: {i.profile}')
-              else:
-                  print('No profile')
-        except Exception as e:
-            # Обрабатываем ошибки
-            print(f"{e}")
-            # Откатываем транзакцию в случае ошибки
-            await session.rollback()
+    return wrapper
 
-    # Явно закрываем соединение с базой данных
-    await engine.dispose()
+# @connection
+# async def add_user_post(session,name,age,email):
+#     user = Person(name=name,age=age,email=email)
+#     session.add(user)
+#     await  session.commit()
+#     return user.id
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+@connection
+async def add_user_and_post(session,name,age,email,title,body):
+    user = Person(name=name,age=age,email=email)
+    session.add(user)
+    await session.commit()
+
+    post = Post(person_id=user.id,title=title,body=body)
+    session.add(post)
+    await session.commit()
+
+
+new_user_and_post = run(add_user_and_post(name='Толя',age=14,
+                                          email='saw@mail.ru',
+                                          title='Название поста',
+                                          body='Описание поста'))
+print(new_user_and_post)
+
+# new_user = run(add_user_post(name='Санёк',age=12,email='saw@mail.ru'))
+# print(new_user)
+
